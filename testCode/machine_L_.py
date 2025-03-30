59bfea820cddd94e35c250de1c11d16eef562bb3
SrLLamaDev <srllamadev@gmail.com>
2025-03-30 18:06:51 -0400

    Coment ESP + ENG

--- testCode/machine_L_.py
+++ testCode/machine_L_.py
@@ -9,15 +9,19 @@ from pynput.keyboard import Controller
 import time
 
 # Crear un controlador de teclado
+# Make a keyboard controller
 keyboard = Controller()
 
 # Esperar un momento antes de presionar la tecla
+# Wait a moment before pressing the key
 time.sleep(2)
 
 # Presionar y soltar la tecla 'L' 100 veces
+# Press and release the 'L' key 100 times
 for _ in range(4000):
     keyboard.press('l')
     keyboard.release('l')
     time.sleep(0.05)  # Pequeña pausa entre cada pulsación
+                        # Small pause between each press
 
 print("Se presionó la tecla 'L' 100 veces")
