261d84522c3f7d6a5d6934311585ea8632c572f5
SrLLamaDev <srllamadev@gmail.com>
2025-04-02 21:17:55 -0400

    practica 1 - ejercicio 2

--- practica_1/Secuenciales/Ejercicio_2_v1.py
+++ practica_1/Secuenciales/Ejercicio_2_v1.py
@@ -1,3 +1,14 @@
+'''
+Para el c´alculo del cobro de un prestamo, se pide que para la cuantificacion del pago se tome
+en cuenta el dinero prestado, el interes anual y los a˜nos en los que la deuda se cancela.
+'''
+'''
+Entrada de datos
+500 10 4
+Ejemplo de salida
+5000.0
+'''
+
 # Entrada de datos
 P, r, t = map(float, input().split())
 
