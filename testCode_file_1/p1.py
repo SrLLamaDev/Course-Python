7c67402f3d5e49d31fb1a7245d1c9344d5dd719c
SrLLamaDev <srllamadev@gmail.com>
2025-04-03 19:59:07 -0400

    testCode

--- testCode_file_1/p1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-##creacion de la matriz
-##matriz = [[0 for _ in range(3)]for _ in range(3)]
-
-##for fila in matriz:
-##    print (fila)
-A = np.array([['a','b','c'],['d','e','f'],['g','h','i']])
-print (A)
